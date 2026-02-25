a05b51e070d3fa33cf984a55a46f4b11bfd0ac6c
sebastien cossus <sebastien.cossus@gmail.com>
2026-02-25 14:14:34 +0100

    end of projet with add swagger

--- src/main/java/com/example/ExampleApplication.java
+++ src/main/java/com/example/ExampleApplication.java
@@ -3,7 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ExampleApplication {
 
     public static void main(String[] args) {
